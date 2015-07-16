d459131a747e8980c4b1a43e328b4f62245559b3
Vladimir <vlad1m1r1712@gmail.com>
2015-07-16 14:44:13 +0300

    12312

--- app/src/main/java/com/example/vladimir/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vladimir/myapplication/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int a = 1;
     }
 
     @Override
